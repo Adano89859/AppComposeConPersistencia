94f9e43a48da48b8f180d025220842f2c1495b0e
Adán Romero <adanromeromarrero@gmail.com>
2025-11-21 00:12:56 +0100

    Terminada esta rama, con todo lo referente al modelo.

--- app/src/main/java/com/example/appcomposeconpersistencia/modelo/BaseDatos.kt
+++ app/src/main/java/com/example/appcomposeconpersistencia/modelo/BaseDatos.kt
@@ -1,16 +1,18 @@
 package com.example.appcomposeconpersistencia.modelo
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@DataBase(entities = [NotaEntity::class], version = 1)
+//Declaro la base de datos y qué tabla tendrá(la de notas)
+@Database(entities = [NotaEntity::class], version = 1)
 abstract class BaseDatos : RoomDatabase() {
-
+    //Indico el Dao a usar para poder trabajar con las acciones CRUD de las notas tanto para base de datos como para en la app
+    abstract fun NotaDao(): NotaDao
+    //Compartimento para indicar utilidades en la clase, me es necesario para poder crear la base de datos
     companion object {
-
+        //Creo la base de datos
         fun create(context: Context) = Room.databaseBuilder(context, BaseDatos::class.java, "Notas.db").build()
-
     }
-
 }
